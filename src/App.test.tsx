99cd870141d9ba819a7548de24ac490693c43876
orbis <infectores@gmail.com>
2022-08-03 12:45:49 +0300

    clear branch

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { App } from "./App";
-import { render } from '@testing-library/react';
-
-describe('App', () => {
-  it('should render coordinates', () => {
-    render(<App />);
-  })
-})
